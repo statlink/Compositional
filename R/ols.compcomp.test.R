2a89bcefb57cb820c74f5b06b8ee0c6576fba354
Michail Tsagris <mtsagris@yahoo.gr>
2023-10-29 09:46:53 +0200

    change

---
+++ R/ols.compcomp.test.R
@@ -0,0 +1,12 @@
+ols.compcomp.test <- function(y, x, B = 999) {
+  
+  stat <- ols.compcomp(y, x)$mse
+  tb <- numeric(B)
+  n <- dim(y)[1]
+  for ( i in 1:B ) {
+    id <- Rfast2::Sample.int(n, n)
+    tb[i] <- ols.compcomp(y, x[id, ])$mse
+  }
+  ( sum(tb > stat) + 1 ) / (B + 1)
+}  
+  
