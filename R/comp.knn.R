dcbad63f52e113c31ddc7a1979829e912cc6b625
Michail Tsagris <mtsagris@yahoo.gr>
2023-12-08 12:44:58 +0200

    change

--- R/comp.knn.R
+++ R/comp.knn.R
@@ -23,13 +23,13 @@ comp.knn <- function(xnew, x, ina, a = 1, k = 5, apostasi = "ESOV", mesos = TRUE
         za <- xnew^a
         znew <- za / Rfast::rowsums( za )  ## The power transformation is applied
       }
-	    g <- Rfast::knn(znew, ina, zx, k = k, dist.type = "manhattan", type = "C", freq.option = 1)
+	g <- Rfast::knn(znew, ina, zx, k = k, dist.type = "manhattan", type = "C", freq.option = 1)
     } else if ( apostasi == "Ait" ) {
       xa <- Rfast::Log(x)
       zx <- xa - Rfast::rowmeans( xa )
       za <- Rfast::Log(xnew)
       znew <- za - Rfast::rowmeans( za )
-	    g <- Rfast::knn(znew, ina, zx, k = k, dist.type = "euclidean", type = "C", freq.option = 1)
+	g <- Rfast::knn(znew, ina, zx, k = k, dist.type = "euclidean", type = "C", freq.option = 1)
 
     } else if ( apostasi == "Hellinger" ) {
       g <- Rfast::knn(sqrt(xnew), ina, sqrt(x), k = k, dist.type = "euclidean", type = "C", freq.option = 1)
@@ -48,24 +48,6 @@ comp.knn <- function(xnew, x, ina, a = 1, k = 5, apostasi = "ESOV", mesos = TRUE
       disa[disa >= 1] <- 1
       disa[ disa <=  -1 ] <-  -1
       disa <- acos(disa)
-
-    } else if ( apostasi == "ESOV" ) {
-	    if ( is.null(a) ) {
-        zx <- x
-        znew <- xnew
-	    } else {
-        xa <- x^a
-        zx <- xa / Rfast::rowsums( xa )  ## The power transformation is applied
-        za <- xnew^a
-        znew <- za / Rfast::rowsums( za )  ## The power transformation is applied
-	    }
-      tzx <- t(zx)
-      disa <- matrix( nrow = nu, ncol = dim(tzx)[2] )
-      for (i in 1:nu) {
-        zan <- znew[i, ]
-        ma <- 0.5 * ( tzx + zan )
-        disa[i, ] <- colSums( zan * log( zan / ma ) + tzx * log( tzx/ma ), na.rm = TRUE )
-      }
 
     } else if ( apostasi == "CS" ) {
       if ( is.null(a) ) {
@@ -86,7 +68,26 @@ comp.knn <- function(xnew, x, ina, a = 1, k = 5, apostasi = "ESOV", mesos = TRUE
         disa[i, ] <- Rfast::colsums( sa )
       }  ## end for (i in 1:nu)
         ## disa <- sqrt(disa) / abs(a) * sqrt(2 * p) not necessary to take the sqrt and then divide and multiply with constants everywhere
-    }  ## end if (apostasi == "CS")
+
+    } else if ( apostasi == "ESOV" ) {
+	if ( is.null(a) ) {
+        zx <- x
+        znew <- xnew
+	} else {
+        xa <- x^a
+        zx <- xa / Rfast::rowsums( xa )  ## The power transformation is applied
+        za <- xnew^a
+        znew <- za / Rfast::rowsums( za )  ## The power transformation is applied
+	}
+      tzx <- t(zx)
+      disa <- matrix( nrow = nu, ncol = dim(tzx)[2] )
+      for (i in 1:nu) {
+        zan <- znew[i, ]
+        ma <- 0.5 * ( tzx + zan )
+        disa[i, ] <- colSums( zan * log( zan / ma ) + tzx * log( tzx/ma ), na.rm = TRUE )
+      }
+
+    }  ## end if ( apostasi == "ESOV" )
 
     disa <- Rfast::rowOrder(disa)
     for (j in 1:klen) {
