a1e217bb6b065593f2d5177794d135bdae5aac14
Michail Tsagris <mtsagris@yahoo.gr>
2024-03-18 18:11:48 +0200

    change

--- R/ternary.cr.R
+++ R/ternary.cr.R
@@ -1,17 +1,17 @@
-cr.ternary <- function(x, type = "alr", conf = 0.95, dg = FALSE, hg = FALSE, colour = NULL) {
-  
+ternary.cr <- function(x, type = "alr", conf = 0.95, dg = FALSE, hg = FALSE, colour = NULL) {
+
   if ( !is.null( colnames(x) ) ) {
     nam <- colnames(x)
   } else nam <- paste("X", 1:3, sep = "")
-  
+
   if (type == "alr") {
     y <- Compositional::alr(x)
   } else  y <- Compositional::alfa(x, 0)
-  n <- dim(x)[1]  ;   p <- 2 
+  n <- dim(x)[1]  ;   p <- 2
   s <- cov(y)
   m <- Rfast::colmeans(y)
   theta <- seq(0, 2 * pi, length = 100)
-  cr <- m + sqrt( (n - 1) * p / (n - p) / n * qf(conf, p, n - p) ) * 
+  cr <- m + sqrt( (n - 1) * p / (n - p) / n * qf(conf, p, n - p) ) *
         t( chol(s) ) %*% rbind( cos(theta), sin(theta) )
   cr <- t(cr)
   cr <- rbind(m, cr)
@@ -21,7 +21,7 @@ cr.ternary <- function(x, type = "alr", conf = 0.95, dg = FALSE, hg = FALSE, col
   } else {
     scr <- Compositional::alfainv(cr, 0)
   }
-  
+
   ## the next code checks for zeros
   b1 <- c(0.5, 0, 1, 0.5)
   b2 <- c(sqrt(3)/2, 0, 0, sqrt(3)/2)
@@ -38,7 +38,7 @@ cr.ternary <- function(x, type = "alr", conf = 0.95, dg = FALSE, hg = FALSE, col
   text( b[1, 1], b[1, 2] + 0.02, nam[3], cex = 1.1 )
   text( b[2, 1] + 0.02, b[2, 2] - 0.02, nam[1], cex = 1.1 )
   text( b[3, 1] - 0.02, b[2, 2] - 0.02, nam[2], cex = 1.1 )
-    
+
   if ( dg ) {
     a1 <- matrix(0, nrow = 11, ncol = 3)
     a1[, 2] <- seq(0, 1, by = 0.1)
