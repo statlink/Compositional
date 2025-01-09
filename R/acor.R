4e8bfd8e1b3177b3ab0e306c83e05dd2d54ca4d6
Michail Tsagris <mtsagris@yahoo.gr>
2025-01-09 19:08:50 +0200

    change

--- R/acor.R
+++ R/acor.R
@@ -67,35 +67,35 @@ acor <- function(y, x, a, type = "dcor") {
           if ( !is.null(ep) )  res[i] <- ep
       }  ##  end  for ( i in 1:length(a) ) {
 
-      } else if ( type =="dcor" ) {
-        res <- numeric( length(a) )
-        names(res) <- paste("alpha=", a, sep = "")
-        for ( i in 1:length(a) ) {
-          res[i] <- Rfast::dcor( alfa(y, a[i])$aff, alfa(x, a[i])$aff )$dcor
-        }
+    } else if ( type =="dcor" ) {
+      res <- numeric( length(a) )
+      names(res) <- paste("alpha=", a, sep = "")
+      for ( i in 1:length(a) ) {
+        res[i] <- Rfast::dcor( alfa(y, a[i])$aff, alfa(x, a[i])$aff )$dcor
+      }
 
-      } else {
-        res <- matrix(NA, nrow = 2, ncol = length(a) )
-        colnames(res) <- paste("alpha=", a, sep = "")
-        rownames(res) <- c("dcor", "cancor")
+    } else {
+      res <- matrix(NA, nrow = 2, ncol = length(a) )
+      colnames(res) <- paste("alpha=", a, sep = "")
+      rownames(res) <- c("dcor", "cancor")
 
-        for ( i in 1:length(a) ) {
-          y1 <- Compositional::alfa(y, a[i])$aff
-          x1 <- Compositional::alfa(x, a[i])$aff
-          res[1, i] <- Rfast::dcor(y1, x1)$dcor
-          ep <- NULL
-          qy1 <- qr(y1)
-          dy1 <- qy1$rank
-          if ( dy1 > 0 )  {
-            qx1 <- qr(x1)
-            dx1 <- qx1$rank
-            if ( dx1 > 0 )  {
-              ep <- svd(qr.qty(qx1, qr.qy( qy1, diag(1, nr, dy)) )[1L:dx, ,drop = FALSE], dx1, dy1)$d[1]
-            }
-          }  ##  end  if ( dy1 > 0 ) {
+      for ( i in 1:length(a) ) {
+        y1 <- Compositional::alfa(y, a[i])$aff
+        x1 <- Compositional::alfa(x, a[i])$aff
+        res[1, i] <- Rfast::dcor(y1, x1)$dcor
+        ep <- NULL
+        qy1 <- qr(y1)
+        dy1 <- qy1$rank
+        if ( dy1 > 0 )  {
+          qx1 <- qr(x1)
+          dx1 <- qx1$rank
+          if ( dx1 > 0 )  {
+            ep <- svd(qr.qty(qx1, qr.qy( qy1, diag(1, nr, dy)) )[1L:dx, ,drop = FALSE], dx1, dy1)$d[1]
+          }
+        }  ##  end  if ( dy1 > 0 ) {
           if ( !is.null(ep) )  res[2, i] <- ep
         }
-      }  ##  end  if ( type == "cancor" ) {
+    }  ##  end  if ( type == "cancor" ) {
 
   }  ##  end   if ( length(a) == 1 )  {
 
