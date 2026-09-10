d74b761cb5187a49193c4178a7ecf97240e162c5
Michail Tsagris <mtsagris@yahoo.gr>
2026-09-10 16:02:31 +0300

    change

--- R/akern.reg.R
+++ R/akern.reg.R
@@ -11,12 +11,14 @@ akern.reg <- function(xnew, y, x, a = seq(0.1, 1, by = 0.1), h = seq(0.1, 1, len
   for ( i in 1:la ) {
     if ( abs( a[i] ) < 1e-6 ) {
       ua <- Compositional::alfa(y, 0, h = FALSE)$aff
-      es <- kernreg::kern_reg(xnew, ua, x, h = h, type = type)
-      for ( j in 1:nh )  est[[ i ]][[ j ]] <- es[[ j ]] / Rfast::rowsums(es[[ j ]])
+      es <- kernreg::kern_reg(xnew, ua, x, h = h, type = type, ncores = ncores)
+      for ( j in 1:nh )  est[[ i ]][[ j ]] <- Compositional::alfainv(es[[ j ]], 0, h = FALSE)
     } else {
       ua <- y^a[i]
       es <- kernreg::kern_reg(xnew, ua, x, h = h, type = type, ncores = ncores)
-      for ( j in 1:nh )  est[[ i ]][[ j ]] <- es[[ j ]] / Rfast::rowsums(es[[ j ]])
+      for ( j in 1:nh )  {
+        es[[ j ]] <- es[[ j ]]^(1/a[i])
+        est[[ i ]][[ j ]] <- es[[ j ]] / Rfast::rowsums(es[[ j ]])
     }
   }  ##  end  for ( i in 1:la ) {
 
